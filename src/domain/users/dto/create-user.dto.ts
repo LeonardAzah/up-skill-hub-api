f6b4ae71bc3b6ca84248c0916332449279d557b1
LeonardAzah <isongleonard@gmail.com>
2024-10-14 12:19:09 +0100

    project initialization

---
+++ src/domain/users/dto/create-user.dto.ts
@@ -0,0 +1,20 @@
+import { IsEmail, IsOptional, IsString, Length } from 'class-validator';
+
+export class CreateUserDto {
+  @Length(2, 50)
+  readonly name: string;
+
+  @IsEmail()
+  readonly email: string;
+
+  @IsString()
+  readonly password: string;
+
+  @IsOptional()
+  @IsString()
+  readonly profile: string;
+
+  @IsOptional()
+  @IsString()
+  readonly role: string;
+}
