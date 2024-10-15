281285c51bf8b41ced883c6f95f6a1b4fad9a710
LeonardAzah <isongleonard@gmail.com>
2024-10-15 16:29:29 +0100

    add logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './domain/users/users.module';
 import { CommonModule } from './common/common.module';
+import { AuthModule } from './auth/auth.module';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
-  imports: [UsersModule, CommonModule],
+  imports: [
+    UsersModule,
+    CommonModule,
+    AuthModule,
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            singleLine: true,
+          },
+        },
+      },
+    }),
+  ],
   controllers: [],
   providers: [],
 })
