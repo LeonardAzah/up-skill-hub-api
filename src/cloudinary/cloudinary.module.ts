5a07862ec34b41c4fae8f1cb8cbc2001b206270f
LeonardAzah <isongleonard@gmail.com>
2024-11-09 21:36:16 +0100

    implement cloudinary service and provider

--- src/cloudinary/cloudinary.module.ts
+++ src/cloudinary/cloudinary.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CloudinaryService } from './cloudinary.service';
+import { CloudinaryProvider } from './config/cloudinary.config';
+import { CommonModule } from 'common/common.module';
 
 @Module({
-  providers: [CloudinaryService]
+  imports: [CommonModule],
+  providers: [CloudinaryService, CloudinaryProvider],
+  exports: [CloudinaryService, CloudinaryProvider],
 })
 export class CloudinaryModule {}
--- src/cloudinary/cloudinary.service.ts
+++ src/cloudinary/cloudinary.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { CloudinaryResponse } from './cloudinary-response';
+import * as streamifier from 'streamifier';
+import { v2 } from 'cloudinary';
 
 @Injectable()
-export class CloudinaryService {}
+export class CloudinaryService {
+  protected readonly logger = new Logger(CloudinaryService.name);
+
+  async uploadFile(
+    file: Express.Multer.File,
+    folder: string,
+  ): Promise<CloudinaryResponse> {
+    return new Promise((resolve, reject) => {
+      const upload = v2.uploader.upload_stream({ folder }, (error, result) => {
+        if (error) return reject(error);
+        resolve(result);
+      });
+      streamifier.createReadStream(file.buffer).pipe(upload);
+    });
+  }
+}
